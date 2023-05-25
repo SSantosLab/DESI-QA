f016bead008c7df77b7c311cb86bc002df3e4918
seanmacb <sean.p.macbride@gmail.com>
2023-05-25 18:15:44 -0400

    Starting with some new movetables, issue with moving hardstop and inadequate moves

--- run_ang.py
+++ run_ang.py
@@ -272,13 +272,13 @@ if __name__=='__main__':
     import argparse
 
     # TODO: remove hardcoded stuff here
-    pix2mm = 0.035406 #0.035337
+    pix2mm = 0.035339 #0.035337
     # todo, pass this as dictionary per positioner
     posid = '4852' # string
-    hardstop_ang = {"4852": -166.270588} # in deg
-    R1 = {"4852": 2.962} # R theta
-    R2 = {"4852": 3.079} # R phi
-    center = {"4852":[ 70.02267, 31.46135]}
+    hardstop_ang = {"4852": -163.07747} # in deg
+    R1 = {"4852": 2.9506121} # R theta
+    R2 = {"4852": 3.0686516} # R phi
+    center = {"4852":[ 69.887, 31.803]}#[ 69.91212, 31.49481]}
 
     pos_speed = {"4852":{"cruise": 33, "spinramp": 12, }}#"spindown": 12}}
     pos_backlash = {"4852": 1.8}
